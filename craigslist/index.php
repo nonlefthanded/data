cc52bcf074df5ff395f86f4b695a71492ff08074
CJ Stritzel <cjstritzel@Admins-MacBook-Pro.local>
2019-03-08 16:52:20 -0800

    adding CL and horoscope data directorys / Initial commit

---
+++ craigslist/index.php
@@ -0,0 +1,25 @@
+<h1>Craigslist Gigs</h1>
+
+<p>Use browser to get Craigslist "gigs" search results, in JSON format.
+
+<h2>Sample URL's</h2>
+<p>The var "city" needs to be a correct Craigslist city subdomain. The var "query" can be whatever you want...
+<ul>
+  <li>
+    <a href="gigs.php">
+      gigs.php (no vars)</a>
+    | Portland | Wordpress
+  </li>
+
+  <li>
+    <a href="gigs.php?city=losangeles&query=react">
+      gigs.php?city=losangeles&query=react</a>
+    | Los Angeles | React
+  </li>
+
+  <li>
+    <a href="gigs.php?city=newyork&query=shopify">
+      gigs.php?city=losangeles&query=react</a>
+    | New York | Shopify
+  </li>
+</ul>
